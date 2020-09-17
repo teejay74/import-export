6c947e77ce01a5be5813f1b85977635a21111eea
teejay74 <teejay744@gmail.com>
2020-09-17 20:13:23 +0500

    task export-import added

---
+++ src/game.js
@@ -0,0 +1,16 @@
+class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+  
+  export class GameSavingData {
+  }
+  
+  export function readGameSaving() {
+  }
+  
+  export function writeGameSaving() {
+  }
+
+  export default Game;
